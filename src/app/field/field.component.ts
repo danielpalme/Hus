c9c515ab68736551a4ab679387bc19af3fbce8ff
Daniel Palme <daniel.palme@gmx.de>
2025-06-15 13:25:47 +0200

    Update Angular 20

--- src/app/field/field.component.ts
+++ src/app/field/field.component.ts
@@ -10,24 +10,28 @@ import { Field } from '../game/game';
       <div>
         <div class="numberofstones">{{ field.numberOfStones }}</div>
       </div>
-      <table class="light" *ngIf="field.moveResult && field.moveResult.numberOfMoves > 0">
-        <tr>
-          <td title="Fields to win"><img src="assets/th-large.svg" width="12" height="12" /></td>
-          <td title="Stones to win"><img src="assets/th.svg" width="12" height="12" /></td>
-          <td title="#Moves"><img src="assets/arrows-h.svg" width="12" height="12" /></td>
-        </tr>
-        <tr>
-          <td title="Fields to win" [ngClass]="{ 'maximum': field.moveResult.isMaximumOfWonOpponentFields }">{{ field.moveResult.numberOfWonOpponentFields }}</td>
-          <td title="Stones to win" [ngClass]="{ 'maximum': field.moveResult.isMaximumOfWonOpponentStones }">{{ field.moveResult.numberOfWonOpponentStones }}</td>
-          <td title="#Moves">{{ field.moveResult.numberOfMoves }}</td>
-        </tr>
-      </table>
-      <div *ngIf="field.moveResult && field.moveResult.numberOfMoves > 0">
-        <br />
-        <a href="" (click)="makeMove($event)" [ngClass]="{ 'maximum': field.moveResult.isMaximumOfWonOpponentFields }">Move</a>
-      </div>
+      @if (field.moveResult && field.moveResult.numberOfMoves > 0) {
+        <table class="light">
+          <tr>
+            <td title="Fields to win"><img src="assets/th-large.svg" width="12" height="12" /></td>
+            <td title="Stones to win"><img src="assets/th.svg" width="12" height="12" /></td>
+            <td title="#Moves"><img src="assets/arrows-h.svg" width="12" height="12" /></td>
+          </tr>
+          <tr>
+            <td title="Fields to win" [ngClass]="{ 'maximum': field.moveResult.isMaximumOfWonOpponentFields }">{{ field.moveResult.numberOfWonOpponentFields }}</td>
+            <td title="Stones to win" [ngClass]="{ 'maximum': field.moveResult.isMaximumOfWonOpponentStones }">{{ field.moveResult.numberOfWonOpponentStones }}</td>
+            <td title="#Moves">{{ field.moveResult.numberOfMoves }}</td>
+          </tr>
+        </table>
+      }
+      @if (field.moveResult && field.moveResult.numberOfMoves > 0) {
+        <div>
+          <br />
+          <a href="" (click)="makeMove($event)" [ngClass]="{ 'maximum': field.moveResult.isMaximumOfWonOpponentFields }">Move</a>
+        </div>
+      }
     </div>
-  `,
+    `,
     styles: [],
     standalone: false
 })
